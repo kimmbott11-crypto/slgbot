a15b660de5afcf033496ebb92b8db1876d0ec09c
ydkim <kimmbott11@gmail.com>
2026-03-02 00:23:28 +0900

    Initial Commit

---
+++ slgbot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  slgbot
+//
+//  Created by 김용덕 on 3/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ slgbot/slgbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  slgbotApp.swift
+//  slgbot
+//
+//  Created by 김용덕 on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct slgbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
